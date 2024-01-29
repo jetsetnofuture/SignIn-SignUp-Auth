7720a0b656a69867c007f69412ff00d458e94fe4
Daniel Carbaugh <daniel.carbaugh142@gmail.com>
2024-01-29 00:04:50 -0500

    Created a boilerplate dashboard component.

--- client/index.js
+++ client/index.js
@@ -7,6 +7,7 @@ import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
+import Dashboard from './components/Dashboard';
 
 const client = new ApolloClient({
   dataIdFromObject: (o) => o.id,
@@ -19,6 +20,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <Route path="login" component={LoginForm} />
           <Route path="signup" component={SignupForm} />
+          <Route path="dashboard" component={Dashboard} />
         </Route>
       </Router>
     </ApolloProvider>
